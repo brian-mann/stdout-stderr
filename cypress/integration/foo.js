7f595d69dcc9cc71eca4ed03cdec81015bf5cc02
Brian Mann <brian.mann86@gmail.com>
2018-05-19 18:48:58 -0400

    dont fail, dont copy

--- cypress/integration/foo.js
+++ cypress/integration/foo.js
@@ -2,10 +2,6 @@ it('passes', () => {})
 
 it('is pending')
 
-it('fails', () => {
-  throw new Error('fail whail')
-})
-
 Array(10).fill().forEach((n, i) => {
   it(`num: ${i+1}`, (done) => {
     setTimeout(done, 10)
